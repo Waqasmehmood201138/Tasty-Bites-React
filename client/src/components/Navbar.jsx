c86c3887c368d599d9fc853c12eb71f1e6e93911
Waqas Mehmood <waqmeh456@gmail.com>
2023-11-26 15:55:10 +0500

    banner section done + navbar sticky

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react'
 import logo from '/logo.png'
 import { BiPhoneCall } from "react-icons/bi";
 
 export default function Navbar() {
+  const [isSticky, setSticky] = useState(false)
+
+  useEffect(() => {
+
+    const handleScroll = () => {
+
+      const offset = window.scrollY;
+      if (offset > 0) {
+        setSticky(true)
+      }
+      else {
+        setSticky(false)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll" , handleScroll)
+    }
+
+  }, [])
 
   const navItems = (
     <>
@@ -34,8 +56,8 @@ export default function Navbar() {
   )
   return (
     <>
-      <header className='max-w-screen-2xl container mx-auto'>
-        <div className="navbar xl:px-24">
+      <header className='max-w-screen-2xl container mx-auto fixed top-0 left-0 right-0 transition-all duration-300 ease-in-out'>
+        <div className={`navbar xl:px-24 ${ isSticky ? "shadow-md bg-base-100 transition-all duration-300 ease-in-out" : ""}`}>
           <div className="navbar-start">
             <div className="dropdown">
               <label tabIndex={0} className="btn btn-ghost lg:hidden">
