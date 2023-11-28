8c73f473535ace8c81cf4730c3fbbed77b220089
Waqas Mehmood <waqmeh456@gmail.com>
2023-11-28 13:47:59 +0500

    filtering and sorting in menu page is completed

--- client/src/Pages/shop/Menu.jsx
+++ client/src/Pages/shop/Menu.jsx
@@ -1,74 +1,175 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import Card from "../../components/Card";
+import { FaFilter } from "react-icons/fa";
 
-export default function Menu() {
+const Menu = () => {
+  const [menu, setMenu] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [sortOption, setSortOption] = useState("default");
 
-    const [menu, setMenu] = useState([]);
-    const [filteredItems, setFilteredItems] = useState([]);
-    const [selectedCategory, setSelectedCategory] = useState("all");
-    const [sortOption, setSortOption] = useState("default");
 
-    // Loadinf data
-    useEffect(() => {
+  useEffect(() => {
+    // Fetch data from the backend
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/menu.json");
+        const data = await response.json();
+        setMenu(data);
+        setFilteredItems(data); // Initially, display all items
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-        // Fetch Data from the Backend
+    fetchData();
+  }, []);
 
-        const fetchData = async () => {
+  const filterItems = (category) => {
+    const filtered =
+      category === "all"
+        ? menu
+        : menu.filter((item) => item.category === category);
 
-            try {
+    setFilteredItems(filtered);
+    setSelectedCategory(category);
 
-                const response = await fetch("/menu.json");
-                const data = await response.json();
-                // console.log(data);
+  };
 
-                setMenu(data)
-                setFilteredItems(data)
-            }
+  const showAll = () => {
+    setFilteredItems(menu);
+    setSelectedCategory("all");
 
-            catch (error) {
+  };
 
-                console.log("Error :", error)
-            }
-        }
+  const handleSortChange = (option) => {
+    setSortOption(option);
 
-        // call the function fetchData
-        fetchData()
-    }, [])
+    // Logic for sorting based on the selected option
+    let sortedItems = [...filteredItems];
 
-    // filtering data based on category
-    const filterItems = (category) => {
-
-        const filtered = category === "all" ? menu : menu.filter((item) => item.category === category)
-
-        setFilteredItems(filtered);
-        setSelectedCategory(category)
+    switch (option) {
+      case "A-Z":
+        sortedItems.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case "Z-A":
+        sortedItems.sort((a, b) => b.name.localeCompare(a.name));
+        break;
+      case "low-to-high":
+        sortedItems.sort((a, b) => a.price - b.price);
+        break;
+      case "high-to-low":
+        sortedItems.sort((a, b) => b.price - a.price);
+        break;
+      default:
+        // Do nothing for the "default" case
+        break;
     }
 
-    return (
-        <>
-            <div>
+    setFilteredItems(sortedItems);
 
-                {/* Menu Banner Section */}
-                <div className="max-w-screen-2xl container mx-auto xl:px-24 bg-gradient-to-r from-0% from-[#FAFAFA] to-[#FCFCFC] to-100%">
-                    <div className="py-48 flex flex-col justify-center items-center gap-8">
-
-                        {/* text */}
-                        <div className='text-center space-y-7 px-4'>
-                            <h2 className='md:text-5xl text-4xl font-bold md:leading-snug leading-snug'>For the Love of Delicious <span className='text-green'>Food</span></h2>
-                            <p className='text-xl text-[#4A4A4A] md:w-4/5 mx-auto'>Come with family & feel the joy of mouthwatering food such as
-                                Greek Salad, Lasagne, Butternut Pumpkin, Tokusen Wagyu, Olivas
-                                Rellenas and more for a moderate cost</p>
-                            <button className='btn bg-green px-8 py-3 font-semibold text-white rounded-full'>Order Now</button>
-                        </div>
+  };
 
 
-                    </div>
-                </div>
 
-                {/* Menu Shop Section */}
-                <div className='max-w-screen-2xl container mx-auto xl:px-24'>
 
-                </div>
+  return (
+    <div>
+      {/* menu banner */}
+      <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4 bg-gradient-to-r from-0% from-[#FAFAFA] to-[#FCFCFC] to-100%">
+        <div className="py-48 flex flex-col items-center justify-center">
+          {/* content */}
+          <div className=" text-center px-4 space-y-7">
+            <h2 className="md:text-5xl text-4xl font-bold md:leading-snug leading-snug">
+              For the Love of Delicious <span className="text-green">Food</span>
+            </h2>
+            <p className="text-[#4A4A4A] text-xl md:w-4/5 mx-auto">
+              Come with family & feel the joy of mouthwatering food such as
+              Greek Salad, Lasagne, Butternut Pumpkin, Tokusen Wagyu, Olivas
+              Rellenas and more for a moderate cost
+            </p>
+            <button className="bg-green font-semibold btn text-white px-8 py-3 rounded-full">
+              Order Now
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* menu shop  */}
+      <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
+        <div className="flex flex-col md:flex-row flex-wrap md:justify-between items-center space-y-3 mb-8">
+          
+           {/* all category buttons */}
+          <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4  flex-wrap">
+            <button
+              onClick={showAll}
+              className={selectedCategory === "all" ? "active" : ""}
+            >
+              All
+            </button>
+            <button
+              onClick={() => filterItems("salad")}
+              className={selectedCategory === "salad" ? "active" : ""}
+            >
+              Salad
+            </button>
+            <button
+              onClick={() => filterItems("pizza")}
+              className={selectedCategory === "pizza" ? "active" : ""}
+            >
+              Pizza
+            </button>
+            <button
+              onClick={() => filterItems("soup")}
+              className={selectedCategory === "soup" ? "active" : ""}
+            >
+              Soups
+            </button>
+            <button
+              onClick={() => filterItems("dessert")}
+              className={selectedCategory === "dessert" ? "active" : ""}
+            >
+              Desserts
+            </button>
+            <button
+              onClick={() => filterItems("drinks")}
+              className={selectedCategory === "drinks" ? "active" : ""}
+            >
+              Drinks
+            </button>
+          </div>
+
+            {/* filter options */}
+          <div className="flex justify-end mb-4 rounded-sm">
+            <div className="bg-black p-2 ">
+              <FaFilter className="text-white h-4 w-4" />
             </div>
-        </>
-    )
-}
+            <select
+              id="sort"
+              onChange={(e) => handleSortChange(e.target.value)}
+              value={sortOption}
+              className="bg-black text-white px-2 py-1 rounded-sm"
+            >
+              <option value="default"> Default</option>
+              <option value="A-Z">A-Z</option>
+              <option value="Z-A">Z-A</option>
+              <option value="low-to-high">Low to High</option>
+              <option value="high-to-low">High to Low</option>
+            </select>
+          </div>
+        </div>
+
+        {/* product card */}
+        <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4 ">
+          {filteredItems.map((item) => (
+            <Card key={item._id} item={item} />
+          ))}
+        </div>
+      </div>
+
+
+    </div>
+  );
+};
+
+export default Menu;
--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -13,7 +13,7 @@ export default function Card({ item }) {
     return (
         <>
          
-            <div className="card shadow-xl relative mr-5 md:my-5">
+            <div to={`/menu/${item._id}`} className="card shadow-xl relative mr-5 md:my-5">
                 <div className={`rating gap-1 absolute right-2 top-2 p-4 heartStar bg-green ${isHeartFilled ? "text-rose-500" : "text-white"}`}
                 onClick={handleHeartClick}
                 >
