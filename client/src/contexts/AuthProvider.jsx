0673af7a99e131c4c79a6ab0be0b64f4d7c93f18
Waqas Mehmood <waqmeh456@gmail.com>
2023-12-10 00:16:18 +0500

    add login signup firebase functions

--- client/src/contexts/AuthProvider.jsx
+++ client/src/contexts/AuthProvider.jsx
@@ -1,12 +1,50 @@
 import React, { createContext, useState } from 'react'
+import { GoogleAuthProvider, getAuth, signInWithPopup, updateProfile } from "firebase/auth";
+import app from '../firebase/firebase.config'
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
 const AuthProvider = ({ children }) => {
 
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true)
+
+    // create account
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    // Signup with gmail
+    const signUpWithGmail = () => {
+      return  signInWithPopup(auth, googleProvider)
+    }
+
+    // login using email and password
+    const login = (email, password) => {
+       return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    // logout
+    const logOut = () => {
+        signOut(auth);
+    }
+
+    // update profile
+    const updateUserProfile = ({name, photoURL}) => {
+      return  updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photoURL
+          })
+    }
 
     const authInfo = {
-        user
+        user,
+        createUser,
+        signUpWithGmail,
+        login,
+        logOut,
+        updateUserProfile
     }
 
     return (
