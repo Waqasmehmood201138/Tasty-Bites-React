0729894a1a112aa58b6d6d1dc04306b8492b082f
Waqas Mehmood <waqmeh456@gmail.com>
2023-12-09 14:33:43 +0500

    react-hook-form setup done for login

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,26 +1,48 @@
 import React from 'react'
 import { FaFacebookF, FaGithub, FaGoogle } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { useForm } from "react-hook-form"
 
 export default function Modal() {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => console.log(data)
+
     return (
         <>
             <dialog id="my_modal_5" className="modal modal-middle sm:modal-middle">
                 <div className="modal-box">
                     <div className="modal-action flex flex-col justify-center mt-0">
-                        <form className="card-body" method="dialog">
+                        <form className="card-body" method="dialog" onSubmit={handleSubmit(onSubmit)}>
                             <h3 className='font-bold text-lg'>Please Login!</h3>
+
+                            {/* Email */}
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input
+                                    type="email" placeholder="email" className="input input-bordered"
+                                    required
+                                    {...register("Email")}
+                                />
                             </div>
+
+                            {/* Password */}
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input
+                                    type="password" placeholder="password" className="input input-bordered"
+                                    required
+                                    {...register("Password")}
+                                />
                                 <label className="label mt-1">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
